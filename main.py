aba232eea5116d22955db203bdbafd2eaf045dba
Gleb Pimenov <gleb.pimenov7@gmail.com>
2022-07-29 14:28:43 +0300

    Работа с .xlsx таблицей

--- main.py
+++ main.py
@@ -1,19 +1,49 @@
+import time
+
 import telebot
+import openpyxl
 import os
 
 def init_bot(): # инициализация бота
     print("Bot_init")
+
     '''
     with open("bot_token.txt", "r") as file:
         telegram_token = file.readline()
         global bot
         bot = telebot.TeleBot(telegram_token)
     '''
-    
+
     telegram_token = os.environ.get('TOKEN')
     global bot
     bot = telebot.TeleBot(telegram_token)
-    
+
+
+def mark_a_person(message): # отметить человека в excel-таблице
+    group = "Group n1307"
+    try:
+        try:
+            book = openpyxl.load_workbook(filename="attendance.xlsx")
+        except FileNotFoundError:
+            book = openpyxl.Workbook()
+        try:
+            sheet = book[group]
+        except KeyError:
+            book.create_sheet(group)
+            sheet = book[group]
+        x, y = 2, 1
+        while sheet.cell(row=y, column=x).value is not None:
+            if sheet.cell(row=y, column=1).value == message.from_user.first_name:
+                while sheet.cell(row=y, column=x).value is not None:
+                    x += 1
+            else:
+                y += 1
+        sheet.cell(row=y, column=1, value=message.from_user.first_name)
+        sheet.cell(row=y, column=x, value=time.ctime(time.time()))
+        book.save("attendance.xlsx")
+    except PermissionError:
+        bot.send_message(message.chat.id, 'Ведутся технические работы, вы не отмечены')
+
 if __name__ == "__main__":
     bot = None
     init_bot()
@@ -26,7 +56,32 @@ def statistic_command(message): # запрос местоположения
     Geo.add(button_geo) # добавление кнопки
     # создаём объект закрытия кастомной клавиатуры
     Geo_close = telebot.types.ReplyKeyboardRemove()
-    bot.send_message(message.chat.id, 'Поделитесь местоположением или введите адрес', reply_markup=Geo )
+    bot.send_message(message.chat.id, 'Поделитесь местоположением', reply_markup=Geo )
+
+@bot.message_handler(commands=['statistics', 'students', 'info'])
+def statistic_command(message): # запрос местоположения
+    group = "Group n1307"
+    try:
+        book = openpyxl.load_workbook(filename="attendance.xlsx")
+    except FileNotFoundError:
+        bot.send_message(message.chat.id, 'Данные о посещаемости не найдены')
+    try:
+        sheet = book[group]
+        x, y = 1, 1
+        text = "Данные:\n"
+        while sheet.cell(row=y, column=x).value is not None:
+            while sheet.cell(row=y, column=x).value is not None:
+                if x != 1:
+                    text += "\t"
+                text += str(sheet.cell(row=y, column=x).value) + "\n"
+                x += 1
+            y += 1
+            x = 1
+            text += "\n"
+        bot.send_message(message.chat.id, text)
+    except KeyError:
+        bot.send_message(message.chat.id, 'Данные о группе "' + group + '" не найдены')
+
 
 @bot.message_handler(content_types=['location'])
 def handle_loc(message):
@@ -43,13 +98,17 @@ def handle_loc(message):
         #print(bot.send_document(message.chat.id, file).id)
     #print(message.id)
     '''
+    epsilon = 0.005
     # проверка местоположения
-    if round(longitude, 3) == 30.297 and round(latitude, 3) == 59.972: #59.97168732680863, 30.296766628835186
+    if (30.2967 - epsilon < round(longitude, 4) < 30.2967 + epsilon) and (59.9717 - epsilon < round(latitude, 4) < 59.9717 + epsilon): #59.97168732680863, 30.296766628835186
         text = "Молодец, " + str(message.from_user.first_name) + ", ходи на пары в 6 корпус дальше!"
-    elif round(longitude, 3) == 30.323 and round(latitude, 3) == 59.972: #59.97225610894957, 30.322730700399283
+        mark_a_person(message)
+    elif (30.3227 - epsilon < round(longitude, 4) < 30.3227 + epsilon) and (59.9723 - epsilon < round(latitude, 4) < 59.9723 + epsilon): #59.97225610894957, 30.322730700399283
         text = "Молодец, " + str(message.from_user.first_name) + ", ходи на пары дальше!"
+        mark_a_person(message)
     else:
         text = "ААА, " + str(message.from_user.first_name) + "!!! ТЫ НЕ В ЛЭТИ!!!"
+        # mark_a_person(message) # Для тестирования
     bot.send_message(message.chat.id, text)
 
 bot.polling()
